9ea50ff679b35e4e6dba9ca1eb1b51266471b47b
Damian Vitale <damian.vitale@globant.com>
2016-03-03 17:15:50 -0300

    update composer

---
+++ src/Car.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Globant\SharedModels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'cars';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password', 'google', 'facebook', 'avatar'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+}
